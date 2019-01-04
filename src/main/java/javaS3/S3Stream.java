e2089f0d3a9184531e67d336eb061131413c2287
dmingus <dmingus@dan.mingus@digitalglobe.com>
2019-01-04 07:49:45 -0700

    fjdkslaa

--- src/main/java/javaS3/S3Stream.java
+++ src/main/java/javaS3/S3Stream.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.log4j.Logger;
+
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
@@ -13,6 +15,8 @@ import com.amazonaws.services.s3.model.S3Object;
 
 public class S3Stream 
 {
+	private static final Logger log = Logger.getLogger( S3Stream.class );
+
 	private final int IO_BUFFER_SIZE = 32 * 1024; // 32KB
 		
 	final static AmazonS3 	s3;
